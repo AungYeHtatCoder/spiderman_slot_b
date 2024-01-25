cc27e183d9f535357abe7129aef49e81c3dcb833
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-01-25 09:38:16 +0630

    a

--- src/components/User/Categories.jsx
+++ src/components/User/Categories.jsx
@@ -4,28 +4,42 @@ import BASE_URL from "../../hooks/baseURL";
 
 const Categories = ({ activeCategory, setActiveCategory, link }) => {
   let [url, setUrl] = useState(link);
-  let btnUrl = BASE_URL + "/gamelist";
+  let btnUrl = BASE_URL + "/gameTypes";
   let { data: games } = useFetch(btnUrl);
 
   return (
     <>
       <div className="categories gap-3 d-flex align-items-center justify-content-center">
-        {games && (
+        {games && games.map((game, index) => {
+          <>
+          <div
+                onClick={() => {
+                  setActiveCategory(game.description),
+                    setUrl(BASE_URL + "/providerCodes/" + game.id);
+                }}
+                className="category "
+              >
+                <img className="categoryImg" src={game.icon} />
+                <p className="font-weight-bold">{game.description}</p>
+          </div>
+          </>
+        })}
+        {/* {games && (
               <>
               <div
                 onClick={() => {
-                  setActiveCategory(games[2]?.description),
+                  setActiveCategory(game.description),
                     setUrl(BASE_URL + "/providerCodes/" + games[0]?.id);
                 }}
                 className="category "
               >
-                <img className="categoryImg" src={games[2]?.icon} />
-                <p className="font-weight-bold">{games[2]?.description}</p>
+                <img className="categoryImg" src={game.icon} />
+                <p className="font-weight-bold">{game.description}</p>
               </div>
               <div
                 onClick={() => {
                   setActiveCategory(games[1]?.description),
-                    setUrl(BASE_URL + "/providerCodes/" + games[2]?.id);
+                    setUrl(BASE_URL + "/providerCodes/" + game.id);
                 }}
                 className="category "
               >
@@ -43,7 +57,7 @@ const Categories = ({ activeCategory, setActiveCategory, link }) => {
                 <p className="font-weight-bold">{games[0]?.description}</p>
               </div>
               </>
-        )}
+        )} */}
       </div>
     </>
   );
