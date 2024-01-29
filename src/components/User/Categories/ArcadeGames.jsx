70e17f752e1df3a4e1b683fa4cecfbf4f4a4f952
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-01-29 15:40:27 +0630

    a

--- src/components/User/Categories/ArcadeGames.jsx
+++ src/components/User/Categories/ArcadeGames.jsx
@@ -51,193 +51,178 @@ const ArcadeGames = (providers2) => {
   };
   const arcades = [a1, a2, a3, a4, a5, a6, a7, a8];
   return (
-    <div className="px-2 px-sm-4 mt-4">
+    <div className="px-2 px-sm-4">
       {loading && <BtnSpinner />}
-      {/* slots */}
-      <div className="mb-5">
-        <div className="mb-4">
-            {play2 && (
-              <div className="d-flex align-items-sm-center">
-                <img
-                  src={play2.img_url}
-                  alt=""
-                  style={{ width: "50px", height: "50px" }}
-                />
-                <h2 className="ms-2">{play2.description}</h2>
+      <div className="row mb-5">
+        <div>
+          {play2 && (
+            <div className="d-flex align-items-sm-center">
+              <img
+                src={play2.img_url}
+                alt=""
+                style={{ width: "50px", height: "50px" }}
+              />
+              <h2 className="ms-2">{play2.description}</h2>
+            </div>
+          )}
+        </div>
+        {play2.providers &&
+          play2.providers.map((slotProvider, index) => {
+            return (
+              <div
+                key={index}
+                className="col-md-4 col-lg-3 col-6 mb-4"
+              >
+                {auth && (
+                  <Link
+                    className="text-decoration-none"
+                    to={"/games"}
+                    onClick={(e) => {
+                      localStorage.removeItem("provider_id");
+                      localStorage.removeItem("gameType_id");
+                      localStorage.removeItem("title");
+                      localStorage.setItem("provider_id", slotProvider.id);
+                      localStorage.setItem(
+                        "gameType_id",
+                        slotProvider.pivot.game_type_id
+                      );
+                      localStorage.setItem("title", slotProvider.description);
+                    }}
+                  >
+                    <img
+                      src={slotProvider.img_url}
+                      alt={slotProvider.description}
+                      className="img-fluid rounded shadow"
+                      style={{ width: "100%", height: "100%" }}
+                    />
+                    {/* <p className="text-white py-3 text-center">
+                      {slotProvider.description}
+                    </p> */}
+                  </Link>
+                )}
+                {!auth && (
+                  <Link className="text-decoration-none" to={"/login"}>
+                    <img
+                      src={slotProvider.img_url}
+                      alt={slotProvider.description}
+                      className="img-fluid rounded shadow"
+                      style={{ width: "100%", height: "100%" }}
+                    />
+                    {/* <p className="text-white py-3 text-center">
+                      {slotProvider.description}
+                    </p> */}
+                  </Link>
+                )}
               </div>
-            )}
-        </div>
-        <div className="row">
-          {play2.providers &&
-            play2.providers.map((slotProvider, index) => {
-              return (
-                <div
-                  key={index}
-                  className="col-md-3 col-6 mb-4"
-                >
-                  {auth && (
-                    <Link
-                      className="text-decoration-none"
-                      to={"/games"}
-                      onClick={(e) => {
-                        localStorage.removeItem("provider_id");
-                        localStorage.removeItem("gameType_id");
-                        localStorage.removeItem("title");
-                        localStorage.setItem("provider_id", slotProvider.id);
-                        localStorage.setItem(
-                          "gameType_id",
-                          slotProvider.pivot.game_type_id
-                        );
-                        localStorage.setItem("title", slotProvider.description);
-                      }}
-                    >
-                      <img
-                        src={slotProvider.img_url}
-                        alt={slotProvider.description}
-                        className="img-fluid rounded shadow"
-                        style={{ width: "100%", height: "100%" }}
-                      />
-                      {/* <p className="text-white mt-2 text-center">
-                        {slotProvider.description}
-                      </p> */}
-                    </Link>
-                  )}
-                  {!auth && (
-                    <Link className="text-decoration-none" to={"/login"}>
-                      <img
-                        src={slotProvider.img_url}
-                        alt={slotProvider.description}
-                        className="img-fluid rounded shadow"
-                        style={{ width: "100%", height: "100%" }}
-                      />
-                      {/* <p className="text-white mt-2 text-center">
-                        {slotProvider.description}
-                      </p> */}
-                    </Link>
-                  )}
-                </div>
-              );
-            })}
-        </div>
+            );
+          })}
       </div>
-      {/* slots */}
-
-      {/* sportbooks */}
-      <div className="mb-5">
-        <div className="mb-4">
-            {play1 && (
-              <div className="d-flex align-items-sm-center">
-                <img
-                  src={play1.img_url}
-                  alt=""
-                  style={{ width: "50px", height: "50px" }}
-                />
-                <h2 className="ms-2">{play1.description}</h2>
+      <div className="row mb-5">
+        <div>
+          {play1 && (
+            <div className="d-flex align-items-sm-center">
+              <img
+                src={play1.img_url}
+                alt=""
+                style={{ width: "50px", height: "50px" }}
+              />
+              <h2 className="ms-2">{play1.description}</h2>
+            </div>
+          )}
+        </div>
+        {play1.providers &&
+          play1.providers.map((sportProvider, index) => {
+            return (
+              <div
+                key={index}
+                className="col-md-4 col-lg-3 col-6 mb-4"
+              >
+                {auth && (
+                  <div
+                    onClick={() => launchGame(sportProvider.id)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <img
+                      src={sportProvider.img_url}
+                      alt={sportProvider.description}
+                      className="img-fluid rounded shadow"
+                      style={{ width: "100%", height: "100%" }}
+                    />
+                    {/* <p className="text-white py-3 text-center">
+                      {sportProvider.description}
+                    </p> */}
+                  </div>
+                )}
+                {!auth && (
+                  <Link className="text-decoration-none" to={"/login"}>
+                    <img
+                      src={sportProvider.img_url}
+                      alt={sportProvider.name_en}
+                      className="img-fluid rounded shadow"
+                      style={{ width: "100%", height: "100%" }}
+                    />
+                    {/* <p className="text-white py-3 text-center">
+                      {sportProvider.description}
+                    </p> */}
+                  </Link>
+                )}
               </div>
-            )}
-        </div>
-        <div className="row">
-          {play1.providers &&
-            play1.providers.map((sportProvider, index) => {
-              return (
-                <div
-                  key={index}
-                  className="col-md-3 col-6 mb-4"
-                >
-                  {auth && (
-                    <div
-                      onClick={() => launchGame(sportProvider.id)}
-                      style={{ cursor: "pointer" }}
-                    >
-                      <img
-                        src={sportProvider.img_url}
-                        alt={sportProvider.description}
-                        className="img-fluid rounded shadow"
-                        style={{ width: "100%", height: "100%" }}
-                      />
-                      <p className="text-white mt-2 text-center">
-                        {sportProvider.description}
-                      </p>
-                    </div>
-                  )}
-                  {!auth && (
-                    <Link className="text-decoration-none" to={"/login"}>
-                      <img
-                        src={sportProvider.img_url}
-                        alt={sportProvider.name_en}
-                        className="img-fluid rounded shadow"
-                        style={{ width: "100%", height: "100%" }}
-                      />
-                      <p className="text-white mt-2 text-center">
-                        {sportProvider.description}
-                      </p>
-                    </Link>
-                  )}
-                </div>
-              );
-            })}
-        </div>
+            );
+          })}
       </div>
-      {/* sportbooks */}
-      
-
-      {/* live-casino */}
-      <div className="mb-5">
-        <div className="mb-4">
-            {play && (
-              <div className="d-flex align-items-sm-center">
-                <img
-                  src={play.img_url}
-                  alt=""
-                  style={{ width: "50px", height: "50px" }}
-                />
-                <h2 className="ms-2">{play.description}</h2>
+      <div className="row mb-5">
+        <div>
+          {play && (
+            <div className="d-flex align-items-sm-center">
+              <img
+                src={play.img_url}
+                alt=""
+                style={{ width: "50px", height: "50px" }}
+              />
+              <h2 className="ms-2">{play.description}</h2>
+            </div>
+          )}
+        </div>
+        {play.providers &&
+          play.providers.map((liveProvider, index) => {
+            return (
+              <div
+                key={index}
+                className="col-md-4 col-lg-3 col-6 mb-4"
+              >
+                {auth && (
+                  <div
+                    onClick={() => launchGame(liveProvider.id)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <img
+                      src={liveProvider.img_url}
+                      alt={liveProvider.description}
+                      className="img-fluid rounded shadow"
+                      style={{ width: "100%", height: "100%" }}
+                    />
+                    {/* <p className="text-white py-3 text-center">
+                      {liveProvider.description}
+                    </p> */}
+                  </div>
+                )}
+                {!auth && (
+                  <Link className="text-decoration-none" to={"/login"}>
+                    <img
+                      src={liveProvider.img_url}
+                      alt={liveProvider.name_en}
+                      className="img-fluid rounded shadow"
+                      style={{ width: "100%", height: "100%" }}
+                    />
+                    {/* <p className="text-white py-3 text-center">
+                      {liveProvider.description}
+                    </p> */}
+                  </Link>
+                )}
               </div>
-            )}
-        </div>
-        <div className="row">
-          {play.providers &&
-            play.providers.map((liveProvider, index) => {
-              return (
-                <div
-                  key={index}
-                  className="col-md-3 col-6 mb-4"
-                >
-                  {auth && (
-                    <div
-                      onClick={() => launchGame(liveProvider.id)}
-                      style={{ cursor: "pointer" }}
-                    >
-                      <img
-                        src={liveProvider.img_url}
-                        alt={liveProvider.description}
-                        className="img-fluid rounded shadow"
-                        style={{ width: "100%", height: "100%" }}
-                      />
-                      <p className="text-white mt-2 text-center">
-                        {liveProvider.description}
-                      </p>
-                    </div>
-                  )}
-                  {!auth && (
-                    <Link className="text-decoration-none" to={"/login"}>
-                      <img
-                        src={liveProvider.img_url}
-                        alt={liveProvider.name_en}
-                        className="img-fluid rounded shadow"
-                        style={{ width: "100%", height: "100%" }}
-                      />
-                      <p className="text-white mt-2 text-center">
-                        {liveProvider.description}
-                      </p>
-                    </Link>
-                  )}
-                </div>
-              );
-            })}
-        </div>
+            );
+          })}
       </div>
-      {/* live-casino */}
     </div>
   );
 };
